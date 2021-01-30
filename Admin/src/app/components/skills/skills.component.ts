cd9f4854f32e2059e774a9abfc19575ecb5ba349
bartblokhuis <bart@bartblokhuis.com>
2021-01-30 20:07:56 +0100

    Added the skill tables

--- Admin/src/app/components/skills/skills.component.ts
+++ Admin/src/app/components/skills/skills.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+enum SkillType {
+ FrontEnd,
+ BackEnd,
+ Other
+}
+
+interface skill {
+  name: string,
+  icon: string,
+  type: SkillType,
+  displayOrder: number
+}
+
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
@@ -7,6 +20,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
+  skills: skill[] = [ 
+    { name: "Angular", icon: "test", displayOrder: 1, type: SkillType.FrontEnd },
+    { name: "CSS", icon: "test", displayOrder: 0, type: SkillType.FrontEnd },
+    { name: "C#", icon: "test", displayOrder: 1, type: SkillType.BackEnd },
+    { name: "Nopcommerce", icon: "test", displayOrder: 1, type: SkillType.Other },
+].sort((a,b) => a.displayOrder - b.displayOrder)
+
+  frontEndSkills: skill[] = this.skills.filter(skill => skill.type === SkillType.FrontEnd);
+  backEndSkills: skill[] = this.skills.filter(skill => skill.type === SkillType.BackEnd);
+  otherSkills: skill[] = this.skills.filter(skill => skill.type === SkillType.Other);
   constructor() { }
 
   ngOnInit(): void {
