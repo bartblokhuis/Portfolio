b6c0c2d33961030b87ceade8eefe9b0720fd44b9
bartblokhuis <bart@bartblokhuis.com>
2021-01-31 00:14:08 +0100

    Added the projects table

--- Admin/src/app/components/projects/projects.component.ts
+++ Admin/src/app/components/projects/projects.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Project {
+
+  title: string,
+  description: string,
+  image: string,
+  displayOrder: number,
+  githubUrl? :string,
+  demoUrl?: string,
+
+}
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
@@ -7,9 +18,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+  projects: Project[] = [
+    {  title: "Ultimate reddit bot", description: "<p>Ultimate Rmework.</p>", image: "",  displayOrder: 0 },
+    {  title: "Ultimate localization", description: "", image: "", displayOrder: 1 },
+    {  title: "Martian weather", description: "", image: "", displayOrder: 3 },
+    {  title: "Clean download folder", description: "", image: "", displayOrder: 2 },
+  ].sort((a,b) => a.displayOrder - b.displayOrder)
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  editProject(): void {
+  }
+
+  deleteProject(): void {
   }
 
 }
